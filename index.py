736b46501b78f9f80ef375e3decadbee271d1468
andrelbdutra <bazulylbd@gmail.com>
2023-06-16 06:24:41 -0300

    adicionando as impressoes dos somatorios dos vouchers

--- index.py
+++ index.py
@@ -15,16 +15,22 @@ class Parceiro:
         self.lucroVendaPorLink = 0
         self.lucroVendaPorLinkApproved = 0
         self.lucroVendaPorLinkRejected = 0
-        self.somatorioVendasCupons = 0
-        self.cupons = []
+        self.somatorioVendasVouchers = 0
+        self.vouchers = []
+        self.vouchersSUM = {}
+        self.qntVendaPorLink = 0
         
-    def imprime_geral(self):
-        print("Parceiro(a): " + str(self.nome) + " - " + "QntPorLink: " + str(self.qntVendaPorLink) + " - " + "LucroPorLink: " + "$" + str(round(self.lucroVendaPorLink,2)))
-    def imprime_approved(self):
-        print("Parceiro(a): " + str(self.nome) + " - " + "QntPorLink: " + str(self.qntVendaPorLinkApproved) + " - " + "LucroPorLink: " + "$" + str(round(self.lucroVendaPorLinkApproved,2)))
-    def imprime_rejected(self):
-        print("Parceiro(a): " + str(self.nome) + " - " + "QntPorLink: " + str(self.qntVendaPorLinkRejected) + " - " + "LucroPorLink: " + "$" + str(round(self.lucroVendaPorLinkRejected,2)))
-
+    def imprime_geral_links(self):
+        print("Parceiro(a): " + str(self.nome) + " | " + "Qnt_Por_Link: " + str(self.qntVendaPorLink) + " | " + "Valor_Por_Link: " + str("${:,.2f}".format(self.lucroVendaPorLink,2)))
+    def imprime_approved_links(self):
+        print("Parceiro(a): " + str(self.nome) + " | " + "Qnt_Por_Link: " + str(self.qntVendaPorLinkApproved) + " | " + "Valor_Por_Link: " + "$" + str(round(self.lucroVendaPorLinkApproved,2)))
+    def imprime_rejected_links(self):
+        print("Parceiro(a): " + str(self.nome) + " | " + "Qnt_Por_Link: " + str(self.qntVendaPorLinkRejected) + " | " + "Valor_Por_Link: " + "$" + str(round(self.lucroVendaPorLinkRejected,2)))
+    def imprime_somatorio_Vendas_Vouchers(self):
+        print("Parceiro(a): " + str(self.nome) + " | " + "Vouchers: " + str(self.vouchers) + " | " + "Somatorio: " + str("${:,.2f}".format(sum(self.vouchersSUM.values()))))
+    def imprime_somatorio_vendas_cada_voucher(self):
+        for key, value in self.vouchersSUM.items():
+            print("Parceiro(a): " + str(self.nome) + " | " + "Voucher: " + str(key) + " | " + "Somatorio: " + str("${:,.2f}".format(value)))
 
 def ler_csv(nome_arquivo, num_atributos):
     objetos = []
@@ -62,6 +68,19 @@ for linkId in linkIds:
     novo_parceiro = Parceiro(linkId.atributos[0], linkId.atributos[1], linkId.atributos[2])
     parceiros.append(novo_parceiro)
 
+# salva os vouchers de cada parceiro
+for parceiro in parceiros:
+    for voucherCode in voucherCodes:
+        if(parceiro.nome == voucherCode.atributos[0]):
+            parceiro.vouchers.append(voucherCode.atributos[1])
+            parceiro.vouchersSUM[voucherCode.atributos[1]] = 0
+
+# realiza o somatorio dos valores dos vouchers
+for parceiro in parceiros:
+    for key, value in parceiro.vouchersSUM.items():
+        for transacao in transacoes:
+            if(transacao.atributos[3] == key):
+                parceiro.vouchersSUM[key] = parceiro.vouchersSUM[key] + float(transacao.atributos[2])
 
 # pega quantidade de vendas por link (total, approved e rejected) dos parceiros
 for transacao in transacoes:
@@ -76,16 +95,36 @@ for transacao in transacoes:
             parceiro.qntVendaPorLinkRejected = parceiro.qntVendaPorLinkRejected + 1
             parceiro.lucroVendaPorLinkRejected = parceiro.lucroVendaPorLinkRejected + float(transacao.atributos[2])
 
-# imprime a quantidade que cada parceiro vendeu nos seus respectivos links associados e o lucro total
-print("\n" * 15)
-print("__GERAL:__ ")
-for parceiro in parceiros:
-    parceiro.imprime_geral()
-print("----------------------------------------------------------------")
-print("__APPROVED:__ ")
-for parceiro in parceiros:
-    parceiro.imprime_approved()
-print("----------------------------------------------------------------")
-print("__REJECTED:__ ")
-for parceiro in parceiros:
-    parceiro.imprime_rejected()
+# impressão 1
+def impressao_geral_links():
+    print("\n" * 15)
+    print("__VENDAS_POR_PARCEIRO:__ ")
+    for parceiro in parceiros:
+        parceiro.imprime_geral_links()
+
+# impressao extras (não requisitadas, porém relevantes)
+def impressoes_extras():
+    print("----------------------------------------------------------------")
+    print("__APPROVED_TRANSACTIONS:__ ")
+    for parceiro in parceiros:
+        parceiro.imprime_approved_links()
+    print("----------------------------------------------------------------")
+    print("__REJECTED_TRANSACTIONS:__ ")
+    for parceiro in parceiros:
+        parceiro.imprime_rejected_links()
+    print("----------------------------------------------------------------")
+    print("__VOUCHERS_TOTAL_SUM:__ ")
+    for parceiro in parceiros:
+        parceiro.imprime_somatorio_Vendas_Vouchers()
+
+# imprime o somatorio individual de vendas de cada voucher associado aos parceiros
+def impressao_vendas_cada_voucher():
+    print("----------------------------------------------------------------")
+    print("__VOUCHERS_SUM:__ ")
+    for parceiro in parceiros:
+        parceiro.imprime_somatorio_vendas_cada_voucher()
+
+
+impressao_geral_links()
+impressao_vendas_cada_voucher()
+impressoes_extras()
